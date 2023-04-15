7a57965059aa4983e153b78619273e4c042dd086
Ekaterina Rykova <katrykova2002@yandex.ru>
2023-04-15 22:39:13 +0300

    added input customer and saved, show errors

---
+++ src/pages/Customer.js
@@ -0,0 +1,142 @@
+import { Link, useParams, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import NotFound from '../components/NotFound';
+import { baseUrl } from '../shared';
+
+export default function Customer() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [customer, setCustomer] = useState();
+  const [tempCustomer, setTempCustomer] = useState();
+  const [notFound, setNotFound] = useState();
+  const [changed, setChanged] = useState(false);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    if (!customer) return;
+    if (!customer) return;
+
+    let equal = true;
+    if (customer.name !== tempCustomer.name) equal = false;
+    if (customer.industry !== tempCustomer.industry) equal = false;
+
+    if (equal) setChanged(false);
+  });
+
+  useEffect(() => {
+    const url = baseUrl + 'api/customers/' + id;
+    fetch(url)
+      .then((response) => {
+        if (response.status === 404) {
+          setNotFound(true);
+        }
+
+        if (!response.ok) {
+          throw new Error('Something went wrong, try again later');
+        }
+
+        return response.json();
+      })
+      .then((data) => {
+        setCustomer(data.customer);
+        setTempCustomer(data.customer);
+        setError(undefined);
+      })
+      .catch((e) => {
+        setError(e.message);
+      });
+  }, []);
+
+  function updateCustomer() {
+    const url = baseUrl + 'api/customers/' + id;
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(tempCustomer),
+    })
+      .then((response) => {
+        if (!response.ok) throw new Error('something went wrong');
+        return response.json();
+      })
+      .then((data) => {
+        setCustomer(data.customer);
+        setChanged(false);
+        setError(undefined);
+      })
+      .catch((e) => {
+        setError(e.message);
+      });
+  }
+
+  return (
+    <>
+      {notFound ? <p>The customer with id {id} was not found</p> : null}
+      {customer ? (
+        <div>
+          <input
+            className="m-2 block px-2"
+            type="text"
+            value={tempCustomer.name}
+            onChange={(e) => {
+              setChanged(true);
+              setTempCustomer({ ...tempCustomer, name: e.target.value });
+            }}
+          />
+          <input
+            className="m-2 block px-2"
+            type="text"
+            value={tempCustomer.industry}
+            onChange={(e) => {
+              setChanged(true);
+              setTempCustomer({ ...tempCustomer, industry: e.target.value });
+            }}
+          />
+          {changed ? (
+            <>
+              <button
+                className="m-2"
+                onClick={() => {
+                  setTempCustomer({ ...customer });
+                  setChanged(false);
+                }}
+              >
+                Cancel
+              </button>
+              <button onClick={updateCustomer} className="m-2">
+                Save
+              </button>
+            </>
+          ) : null}
+
+          <button
+            onClick={(e) => {
+              const url = baseUrl + 'api/customers/' + id;
+              fetch(url, {
+                method: 'DELETE',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+              })
+                .then((response) => {
+                  if (!response.ok) {
+                    throw new Error('Something went wrong');
+                  }
+                  navigate('/customers');
+                })
+                .catch((e) => {
+                  setError(e.message);
+                });
+            }}
+          >
+            Delete
+          </button>
+        </div>
+      ) : null}
+      {error ? <p>{error}</p> : null}
+      <br />
+      <Link to="/customers">Go back</Link>
+    </>
+  );
+}
--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,3 +1,68 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import AddCustomer from '../components/AddCustomer';
+import { baseUrl } from '../shared';
+
 export default function Customers() {
-  return <h1>Hello there!</h1>;
+  const [customers, setCustomers] = useState();
+  const [show, setShow] = useState(false);
+
+  function toggleShow() {
+    setShow(!show);
+  }
+
+  useEffect(() => {
+    const url = baseUrl + 'api/customers/';
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCustomers(data.customers);
+      });
+  }, []);
+
+  function newCustomer(name, industry) {
+    const data = { name: name, industry: industry };
+    const url = baseUrl + 'api/customers/';
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Something went wrong');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        toggleShow()
+        setCustomers([...customers, data.customer])
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+  return (
+    <>
+      <h1>Here are our customers:</h1>
+      <ul>
+        {customers
+          ? customers.map((customer) => {
+              return (
+                <li key={customer.id}>
+                  <Link to={'/customers/' + customer.id}>{customer.name}</Link>
+                </li>
+              );
+            })
+          : null}
+      </ul>
+      <AddCustomer
+        newCustomer={newCustomer}
+        show={show}
+        toggleShow={toggleShow}
+      />
+    </>
+  );
 }
